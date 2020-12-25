e217a402bc4bb620ddc7cb90eff391a2ca66c5df
NeProgramist <zaskozhenya@gmail.com>
2020-12-25 21:47:46 +0200

    feat(common): add scenario: get info after update

--- client/example.js
+++ client/example.js
@@ -28,11 +28,24 @@ const client = new Clients('localhost', 8080);
     }
 })();
 
-// Scenario 3: Update machine with invalid id
+// Scenario 3: Get info about balancers after update
+(async () => {
+    try {
+        const balancers = await client.getBalancers()
+        console.log('---Scenario 3---')
+        balancers.forEach(balancer => {
+            console.log(balancer)
+        })
+    } catch(err) {
+        console.log(err)
+    }
+})();
+
+// Scenario 4: Update machine with invalid id
 (async () => {
     try {
         const result = await client.updateMachines(-3, true)
-        console.log('---Scenario 3---')
+        console.log('---Scenario 4---')
         console.log(result)
     } catch(err) {
         console.log(err)
