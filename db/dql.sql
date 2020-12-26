613f00953caf701c99cc79df6cc73f9e5442a134
NeProgramist <zaskozhenya@gmail.com>
2020-12-26 21:47:58 +0200

    fix(db): change functions that retrieves balancers info

--- db/dql.sql
+++ db/dql.sql
@@ -1,34 +1,36 @@
-create or replace function get_balancers_id() returns setof integer as '
-    select id from balancers
-' language sql;
-alter function get_balancers_id() owner to balance_admin;
+create or replace function get_balancers_info()
+returns table(id integer, count bigint)
+language sql as $$
+    select distinct b.id,
+    count(*) over(partition by b.id)
+    from balancers b
+    join machines m on b.id = m.balancer_id
+    order by b.id;
+$$;
 
-select get_balancers_id();
+select * from get_balancers_info();
 
-create or replace function get_machines_quantity(balancer integer) returns bigint as '
-    select count(*) from machines
-    where balancer_id = balancer
-' language sql;
-alter function get_machines_quantity(integer) owner to balance_admin;
+create function get_usable_machines()
+returns table(balancer_id integer, id integer)
+language sql as $$
+    select balancer_id, id from machines
+    where state = true
+$$;
 
-select get_machines_quantity(3);
+select * from get_usable_machines();
 
-create or replace function get_usable_machines(balancer integer) returns setof int as '
-    select id from machines
-    where balancer_id = balancer and state = true
-' language sql;
-alter function get_usable_machines(integer) owner to balance_admin;
-
-select get_usable_machines(3);
-
-create or replace procedure update_machine(machine_id integer, s bool) as '
+create or replace procedure update_machine(machine_id integer, s bool)
+language sql as $$
     update machines
     set state = s
     where id = machine_id
-' language sql;
-alter procedure update_machine(integer, boolean) owner to balance_admin;
+$$;
 
 call update_machine(1, false);
+
+alter procedure update_machine(integer, boolean) owner to balance_admin;
+alter function get_balancers_info() owner to balance_admin;
+alter function get_usable_machines() owner to balance_admin;
 
 alter sequence balancers_id_seq owner to balance_admin;
 alter sequence machines_id_seq owner to balance_admin;
