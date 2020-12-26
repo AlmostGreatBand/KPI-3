5566f81aaae26de4222869c164d1bd8e06b4e419
NeProgramist <zaskozhenya@gmail.com>
2020-12-26 21:37:45 +0200

    feat(client): move examples of usage to functions

--- client/example.js
+++ client/example.js
@@ -4,50 +4,42 @@ const Clients = require('./balancers/client');
 
 const client = new Clients('localhost', 8080);
 
-// Scenario 1: Get info about balancers
-(async () => {
+const getBalancersInfo = async () => {
     try {
         const balancers = await client.getBalancers()
-        console.log('---Scenario 1---')
         balancers.forEach(balancer => {
             console.log(balancer)
         })
     } catch(err) {
         console.log(err)
     }
-})();
+}
 
-// Scenario 2: Update existed machine id
-(async () => {
+const updateMachines = async (id, state) => {
     try {
-        const result = await client.updateMachines(1, true)
-        console.log('---Scenario 2---')
+        const result = await client.updateMachines(id, state)
         console.log(result)
     } catch(err) {
         console.log(err)
     }
-})();
+}
 
-// Scenario 3: Get info about balancers after update
-(async () => {
-    try {
-        const balancers = await client.getBalancers()
-        console.log('---Scenario 3---')
-        balancers.forEach(balancer => {
-            console.log(balancer)
-        })
-    } catch(err) {
-        console.log(err)
-    }
-})();
+// Usage:
 
-// Scenario 4: Update machine with invalid id
 (async () => {
-    try {
-        const result = await client.updateMachines(-3, true)
-        console.log('---Scenario 4---')
-        console.log(result)
-    } catch(err) {
-        console.log(err)
-    }
+    // Scenario 1: Get info about balancers
+    console.log('---Scenario 1---')
+    await getBalancersInfo()
+
+    // Scenario 2: Update existed machine id
+    console.log('---Scenario 2---')
+    await updateMachines(1, false)
+
+    // Scenario 3: Get info about balancers after update
+    console.log('---Scenario 3---')
+    await getBalancersInfo()
+
+    // Scenario 4: Update machine with invalid id
+    console.log('---Scenario 4---')
+    await updateMachines(-3, true)
 })();
